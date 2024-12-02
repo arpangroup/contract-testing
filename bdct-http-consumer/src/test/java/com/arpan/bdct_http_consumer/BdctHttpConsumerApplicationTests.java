c1d388a9a5dab62f32d1bc1beac5b1757f81c344
arpan jana <arpangroup1@gmail.com>
2024-12-02 20:39:10 +0530

    initial commit

---
+++ bdct-http-consumer/src/test/java/com/arpan/bdct_http_consumer/BdctHttpConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpan.bdct_http_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdctHttpConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ bdct-http-provider/src/test/java/com/arpan/bdct_http_provider/BdctHttpProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpan.bdct_http_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdctHttpProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
