f23a144afd3c11ad56169a9fbc08e6f6bb5bae24
arpan jana <arpangroup1@gmail.com>
2024-12-08 02:50:05 +0530

    initial commit for cdct-http-provider

---
+++ cdct-http-provider/src/test/java/com/arpan/cdct_http_provider/CdctHttpProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpan.cdct_http_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdctHttpProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
